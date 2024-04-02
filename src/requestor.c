f10c65fd304e61285b0fad80c263b91f37c9c2a7
lostsh <yohann.vernhes@gmail.com>
2024-04-02 16:06:21 +0200

    removing unistd dependecy from requestor

--- src/requestor.c
+++ src/requestor.c
@@ -8,7 +8,6 @@
 */
 
 #include "requestor.h"
-#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
@@ -26,36 +25,39 @@ int getHttpCode(const char *target_ip, char *return_code){
 
     //Connect socket to server
     if(connect(sockfd, (struct sockaddr*)&target, sizeof(target)) < 0){
-        write(STDERR_FILENO, "[ ! ]\tException: connect failed\n", 32);
+        //write(STDERR_FILENO, "[ ! ]\tException: connect failed\n", 32);
         return 1;
     }
 
     //Send HTTP request
     char *reqStr = "HEAD / HTTP/1.0\r\n\r\n";
-    if(write(sockfd, reqStr, len(reqStr)) < 0){
-        write(STDERR_FILENO, "[ ! ]\tException: send failed\n", 29);
+    if(send(sockfd, reqStr, len(reqStr), 0) < 0){
+        //write(STDERR_FILENO, "[ ! ]\tException: send failed\n", 29);
         return 1;
     }
 
     //Handle http response
     char resp_buff[16] = "";
-    int resp_size = read(sockfd, resp_buff, sizeof(resp_buff));
+    int resp_size = recv(sockfd, resp_buff, sizeof(resp_buff), 0);
     if(resp_size <= 0){
-        write(STDERR_FILENO, "[ ! ]\tException: read or socket closed\n", 40);
-        return 1;
+        //write(STDERR_FILENO, "[ ! ]\tException: read or socket closed\n", 40);
+        return resp_size;
     }
     *(resp_buff+resp_size-1) = '\0';
     //write(STDOUT_FILENO, resp_buff, resp_size);
     //write(1, "\n", 1);
 
+    //Colose socket
+    shutdown(sockfd, 2);
+
     //parse output
     int parse_success = parseCode(resp_buff, return_code);
     if(parse_success < 0){
-        write(STDERR_FILENO, "[ ! ]\tExeption: parsing code\n", 29);
-        return 1;
+        //write(STDERR_FILENO, "[ ! ]\tExeption: parsing code\n", 29);
+        return parse_success;
     }
 
-    close(sockfd);
+    //close(sockfd);
     return 0;
 }
 
